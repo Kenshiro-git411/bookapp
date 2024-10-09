20514a6ac9f0c9c4b5efcb8b61f5f345af14b091
Kenshiro-git411 <ichikawa.kenshiro.job303@gmail.com>
2024-10-09 16:56:44 +0900

    modified views.py class SearchBook

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
+from django.views import View
 import requests
 
 # サイトにアクセスした時に表示する画面までのアクセス
@@ -9,16 +10,35 @@ def SearchViewfunc(request):
         return redirect('search')
 
 # 検索結果画面表示のクラス
-class SearchBook(ListView):
-    def get_postData(request):
+class SearchBook(TemplateView):
+    template_name = 'result.html'
+    context_object_name = 'result'
+
+    def post(self, request, *args, **kwargs):
+        # NDL APIのURL
         apiUrl = 'https://ndlsearch.ndl.go.jp/api/opensearch'
-        if request.method == 'POST':
-            input1 = request.POST['searchword']
-            print('データを受け取った！')
-            print(f'入力されたデータ{input1}')
-            searchUrl = apiUrl + '?title=' + '{input1}'
-            print(f'検索時のURL: {searchUrl}')
-            response = requests.get(searchUrl)
-            print(response)
-            return render(request, 'result.html', {'message':response.json()})
+
+        # フォームから入力された検索ワードを取得
+        searchword = request.POST.get('searchword', '')
+
+        # APIの検索URLを作成
+        searchUrl = f'{apiUrl}?title={searchword}'
+        print(f'検索時のURL: {searchUrl}')
+
+        # APIにリクエストを送信
+        response = requests.get(searchUrl)
+
+        if response.status_code == 200:
+            #レスポンスが正常だった場合
+            data = response.json()
+        else:
+            # エラーハンドリング
+            data = {}
+
+        # コンテキストに結果を追加
+        context = self.get_context_data()
+        context['message'] = data
+
+        # 結果をテンプレートに渡して表示
+        return self.render_to_response(context)
 
