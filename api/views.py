70235e285d2502ae171599ec5185a9b82b592272
Kenshiro-git411 <ichikawa.kenshiro.job303@gmail.com>
2024-10-12 11:22:22 +0900

    xmltodict xml->json

--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from urllib.parse import quote
+import xml.etree.ElementTree as ET
+import json, xmltodict
 import requests
 
 # サイトにアクセスした時に表示する画面までのアクセス
@@ -28,18 +30,31 @@ class SearchBook(TemplateView):
 
         # フォームから入力された検索ワードをエンコードする
         encoding_searchword = quote(searchword)
-        print(f'encode後検索ワード: {encoding_searchword}')
+        # print(f'encode後検索ワード: {encoding_searchword}')
 
         # APIの検索URLを作成
         searchUrl = f'{apiUrl}?title={encoding_searchword}'
-        print(f'検索時のURL: {searchUrl}')
+        # print(f'検索時のURL: {searchUrl}')
 
         # APIにリクエストを送信
         response = requests.get(searchUrl)
+        # print(response)
 
+        #レスポンスが正常だった場合
         if response.status_code == 200:
-            #レスポンスが正常だった場合
-            data = response.json()
+            xml_data = response.content
+            # print(xml_data)
+
+            # XMLデータをパースする
+            root = xmltodict.parse(xml_data)
+            # print(root)
+
+            # 辞書をJSON形式に変換
+            data = json.dumps(root, ensure_ascii=False, indent=2)
+
+            # JSONデータを辞書に変換
+            # data = json.loads(data)
+
         else:
             # エラーハンドリング
             data = {}
@@ -47,6 +62,9 @@ class SearchBook(TemplateView):
         # コンテキストに結果を追加
         context = self.get_context_data()
         context['message'] = data
+
+        # for item in data['rss']['channel']['item']:
+        #     print(item['title'],item['author'])
 
         # 結果をテンプレートに渡して表示
         return self.render_to_response(context)
