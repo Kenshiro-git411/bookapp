597b2cbcf3535b7876e9fe6cbea89c95fe169d3f
Kenshiro-git411 <ichikawa.kenshiro.job303@gmail.com>
2024-10-16 20:07:10 +0900

    dict型変換

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SearchBook, SearchViewfunc
+from .views import SearchBook, SearchViewfunc, Signup, Login
 
 urlpatterns = [
     path('', SearchViewfunc),
     path('search/', SearchBook.as_view(), name='search'),
+    path('signup/', Signup.as_view(), name='signup'),
+    path('login/', Login.as_view(), name='login'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
+from django.db import IntegrityError
 from django.http import HttpResponse
 from urllib.parse import quote
 import xml.etree.ElementTree as ET
@@ -21,7 +24,7 @@ class SearchBook(TemplateView):
     context_object_name = 'result'
 
     def post(self, request, *args, **kwargs):
-        print("SearchGBook関数の始まり")
+        print("SearchGBookクラス-post関数の始まり")
         # NDL APIのURL
         apiUrl = 'https://ndlsearch.ndl.go.jp/api/opensearch'
 
@@ -53,7 +56,7 @@ class SearchBook(TemplateView):
             data = json.dumps(root, ensure_ascii=False, indent=2)
 
             # JSONデータを辞書に変換
-            # data = json.loads(data)
+            data = json.loads(data)
 
         else:
             # エラーハンドリング
@@ -62,6 +65,7 @@ class SearchBook(TemplateView):
         # コンテキストに結果を追加
         context = self.get_context_data()
         context['message'] = data
+        print(type(data))
 
         # for item in data['rss']['channel']['item']:
         #     print(item['title'],item['author'])
@@ -69,3 +73,47 @@ class SearchBook(TemplateView):
         # 結果をテンプレートに渡して表示
         return self.render_to_response(context)
 
+
+# サインアップ処理
+class Signup(TemplateView):
+    template_name = 'signup.html'
+    context_object_name = 'signup'
+
+    def post(self, request):
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+
+            try:
+                # 登録の重複がない場合
+                user = User.objects.create_user(username, '', password)
+                print('登録の重複はありません。登録処理を実行済みです。')
+                return render(request, self.template_name, {})
+            except IntegrityError:
+                # 登録が重複した場合
+                print('登録の重複があります。登録できませんでした。')
+                return render(request, self.template_name, {'error':'このユーザーはすでに登録されています'})
+
+        return render(request, self.template_name, {})
+
+# ログイン処理
+class Login(TemplateView):
+    template_name = 'login.html'
+    context_object_name = 'login'
+
+    def post(self, request):
+        if request.method == 'POST':
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                print('ユーザーが登録されていることを確認できました')
+                return redirect('list')
+            else:
+                print('ユーザーが登録されていることを確認できませんでした')
+                return render(request, 'login.html', {'context':'not logged in'})
+
+        # GETメソッドの場合の処理
+        return render(request, self.template_name, {'context':'get method'})
