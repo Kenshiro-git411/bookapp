01db5b11b4d4005815d71161cc2ac9e3f0135ec2
Kenshiro-git411 <ichikawa.kenshiro.job303@gmail.com>
2024-12-31 06:06:55 +0000

    settings.pyの修正、vultrデプロイやり直し

--- djangobook/settings.py
+++ djangobook/settings.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
-from dj_database_url import parse as dburl
+from dj_database_url import parse
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,8 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = config('SECRET_KEY')
 
 # DEBUGについては.envファイルに書いてあるため、configファイル(.envファイル)を指定するように書いておく。
-DEBUG = config('DEBUG') #デプロイ設定
-
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 
 # Application definition
@@ -27,10 +27,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework', # APIフレームワーク
+#    'rest_framework', # APIフレームワーク
     'api.apps.ApiConfig', # APIアプリ
-    'corsheaders', # CORS対応
-    'djoser', # ユーザー認証用
+#    'corsheaders', # CORS対応
+#    'djoser', # ユーザー認証用
 ]
 
 MIDDLEWARE = [
@@ -51,21 +51,21 @@ MIDDLEWARE = [
 
 # JSON Web Token（JWT）認証を実装するためのライブラリ
 # セキュアなユーザー認証を行う
-SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES': ('JWT',), #リクエスト認証ヘッダーで使用されるトークンタイプを示す。
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60), #アクセストークンの有効期限を示し、アクセストークンが発行されてから60分後に無効になることを示す。
-}
+#SIMPLE_JWT = {
+#    'AUTH_HEADER_TYPES': ('JWT',), #リクエスト認証ヘッダーで使用されるトークンタイプを示す。
+#    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60), #アクセストークンの有効期限を示し、アクセストークンが発行されてから60分後に無効になることを示す。
+#}
 
-REST_FRAMEWORK = {
+#REST_FRAMEWORK = {
     #apiエンドポイント（url先）へのアクセスに対するデフォルトの権限クラスを指定する。
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', #全てのエンドポイントに対してユーザーが認証されている必要があることを示す。認証されていないユーザーはエンドポイントにアクセスできないことになっている。
-    ],
+#    'DEFAULT_PERMISSION_CLASSES': [
+#        'rest_framework.permissions.IsAuthenticated', #全てのエンドポイントに対してユーザーが認証されている必要があることを示す。認証されていないユーザーはエンドポイントにアクセスできないことになっている。
+#    ],
     #デフォルトの認証クラスを指定する。
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication', #JWTトークンを用いた認証方式を使用することになる。
-    ],
-}
+#    'DEFAULT_AUTHENTICATION_CLASSES': [
+#        'rest_framework_simplejwt.authentication.JWTAuthentication', #JWTトークンを用いた認証方式を使用することになる。
+#    ],
+#}
 
 ROOT_URLCONF = 'djangobook.urls'
 
@@ -100,7 +100,9 @@ if DEBUG:
         }
     }
 else:
-    DATABASES = {"default": dburl.config('DATABASE_URL')}
+    DATABASES = {
+        'default':parse(config('DATABASE_URL'))
+    }
 
 
 # Password validation
@@ -137,11 +139,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.normpath(os.path.join(BASE_DIR, "assets")),
-)
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+#開発環境
+if DEBUG:
+    STATICFILES_DIRS = (
+        os.path.normpath(os.path.join(BASE_DIR, "assets/")),
+    )
+#本番環境
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
+
+# Mediaファイル
+MEDIA_URL = '/media/'
+# 開発環境
+if DEBUG:
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+# 本番環境
+else:
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -158,7 +173,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'api:top'
 
 # CSRFトークンをチェックする際、指定されたものは信頼あるリクエストとして扱う
-CSRF_TRUSTED_ORIGINS = [ "http://127.0.0.1:8000" ]
+CSRF_TRUSTED_ORIGINS = [ 'http://127.0.0.1:8000', 'http://localhost', ]
 
 # メール送信のバックエンドを指定
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -166,18 +181,21 @@ CSRF_TRUSTED_ORIGINS = [ "http://127.0.0.1:8000" ]
 # カスタムユーザーモデルをデフォルトに設定
 AUTH_USER_MODEL = 'api.User'
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
 
 # ローカル用設定
 if DEBUG:
-    ALLOWED_HOSTS = ['*'] #開発環境ではすべてのホストからのアクセスを許可する。
+    ALLOWED_HOSTS = ['localhost','127.0.0.1',]
+    env = environ.Env()
+    env.read_env(os.path.join(BASE_DIR, '.env'))
+    SECRET_KEY = env('SECRET_KEY')
     # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # メールの内容をコンソールに表示する。
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #djangoappプロジェクトフォルダ配下のmediaフォルダを指定。
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = "smtp.gmail.com" # GmailのSMTPサーバー
+    EMAIL_HOST = 'smtp.gmail.com' # GmailのSMTPサーバー
     EMAIL_PORT = 587 # Gmailサーバーのポート番号
     EMAIL_HOST_USER = config('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
@@ -185,30 +203,17 @@ if DEBUG:
 
 # 本番環境用設定
 if not DEBUG:
-    import environ
     env = environ.Env()
     env.read_env(os.path.join(BASE_DIR, '.env')) #.envファイルの読み込み
 
     SECRET_KEY = env('SECRET_KEY') #.envファイルからsecretkeyを取得。
     ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
-    # 以下に本番環境のSTATIC_ROOTとMEDIA_ROOTを書く。
-    STATIC_ROOT = '/usr/share/nginx/html/static'
-    MEDIA_ROOT = '/usr/share/nginx/html/media'
-
     # メールに関する情報
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = "smtp.gmail.com" # GmailのSMTPサーバー
+    EMAIL_HOST = 'smtp.gmail.com' # GmailのSMTPサーバー
     EMAIL_PORT = 587 # Gmailサーバーのポート番号
     EMAIL_HOST_USER = env('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
     EMAIL_USE_TLS = True # SMTPサーバーと通信する際に、TLS（セキュア）接続する
 
-# メール送信における情報
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = "smtp.gmail.com" # GmailのSMTPサーバー
-# EMAIL_PORT = 587 # Gmailサーバーのポート番号
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-# EMAIL_USE_TLS = True # SMTPサーバーと通信する際に、TLS（セキュア）接続する
